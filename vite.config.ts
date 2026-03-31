b9446fb376c919e44e4547f9230b8f3ebda5c781
TSURUDO Ryosuke <18186312+ROBO358@users.noreply.github.com>
2026-03-31 20:33:59 +0900

    feat: update deployment workflow to use specific commit reference and rename base path variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-	base: process.env.VITE_BASE_PATH ?? "/",
+	base: process.env.BASE_PATH ?? "/",
 	plugins: [react(), wasm(), topLevelAwait()],
 });
